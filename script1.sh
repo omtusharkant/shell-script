e1d3fab67734a78830bfd52c902ccf643ca091ff
Tusharkant <61859664+omtusharkant@users.noreply.github.com>
2024-07-09 23:45:46 +0530

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+<<  COMMENT
+Description : A simple slam book
+Author      : Tommarcusbrut
+Date Created: 9/7/2024
+COMMENT
+
+echo "===================="
+echo "Slam Book"
+echo "===================="
+echo "Enter your name  :"
+read name
+
+echo "Nick Name :"
+read nick
+
+echo "email  :"
+read email
+
+echo "Phone No  :"
+read phone
+
+echo -e "Ok good bye for now $nick i will get in with you with these mediums \nphone :  $phone \nemail : $email"
+
+
